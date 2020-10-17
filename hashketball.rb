abcffe228f2286fc5659186de90bf10c8f81582c
Alfred Wang <alfredwang0707@gmail.com>
2020-10-17 23:58:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,8 @@ game_hash
    away: {
   team_name: "Charlotte Hornets" ,
   colors: ["Turquoise" , "Purple"] ,
-  "players => [  {
+  :players => [ 
+  {
       "Jeff Adrien" =>{
         :number => 4, 
         :shoe => 18,
