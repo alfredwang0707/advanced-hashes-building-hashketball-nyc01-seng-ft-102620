fb0ce4f477325ed6e961f62582635800fddccb67
Alfred Wang <alfredwang0707@gmail.com>
2020-10-18 00:03:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,8 +7,7 @@ game_hash
   :colors => ["black", "white"] ,
   :players => [
      #* The `:players` key points to an `Array` of `Hash`es. Each `Hash` in the `Array`
-    
-   {:name => "Alan Anderson", 
+ {:name => "Alan Anderson", 
   :number => 0,
   :shoe => 16,
   :points => 22, 
